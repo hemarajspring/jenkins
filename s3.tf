72a486fd03b3cf016b2dd4472a9cc2357cb3e765
hemarajspring <81808944+hemarajspring@users.noreply.github.com>
2024-03-06 11:38:28 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -2,14 +2,7 @@ provider "aws" {
   region = "us-east-1" # Specify your desired region
 }
 
-resource "random_string" "bucket_name" {
-  length  = 8
-  special = false
-}
-
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "${random_string.bucket_name.result}"
-
-  # You can add additional configurations as needed
-
+  bucket = "valid-bucket-name-hemaraj-009"
+  # Additional configuration options...
 }
